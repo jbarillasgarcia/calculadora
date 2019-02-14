0c72333dc0c0a66905aec710e7c653dcbf018bb7
Josué Barillas <jbarillasgarcia@gmail.com>
2019-02-13 20:03:08 -0600

    Version inicial de mi calculadora.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Josue
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(add(1,2));
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+}
